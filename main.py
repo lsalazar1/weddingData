44f0b6ab46fc6a82a21d5541e0dd5eb654583124
lsalazar1 <lmsalazar78011@gmail.com>
2020-01-02 04:22:21 -0500

    Working on countGuests function

---
+++ main.py
@@ -0,0 +1,34 @@
+from configparser import ConfigParser
+import pymongo
+import dns
+
+class websiteData:
+    numGuests = 0
+
+    def __init__(self):
+        self.parser = ConfigParser()
+        
+        self.parser.read('default.ini')
+        
+        self.connection = pymongo.MongoClient(self.parser.get('db', 'mongoURI'))
+
+        self.database = self.connection['test']
+        self.collection = self.database['profiles']
+
+    # Counts guests who have accepted their invitations, as well as their guests
+    def countGuests(self):
+        col = list(self.collection.find())
+
+        for doc in col:
+            if doc['status'] == 'Accept':
+                self.numGuests += 1
+                
+                
+        
+        return self.numGuests
+
+
+# Testing Purposes
+a = websiteData()
+
+print(a.countGuests())
